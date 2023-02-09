435cfdc9ae54fbbcd9ae3340ffcb8e37e08ec4ef
Tyler Rankin <tjrankin572@gmail.com>
2023-02-08 19:57:25 -0800

    Monkey again!

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,6 @@ module.exports = {
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
-    `gatsby-plugin-fontawesome-css`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     {
