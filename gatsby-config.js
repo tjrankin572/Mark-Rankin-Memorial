2fb91ddd408b540fad17fddcab225e8c5e04a98d
Tyler Rankin <tjrankin572@gmail.com>
2023-02-13 19:49:01 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Mark Rankin Memorial`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
